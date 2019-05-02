a95235b97fc86ae023624d249f55102e1a634b36
justflying <jonglae@gmail.com>
2019-05-03 05:54:04 +0900

    Initial commit

---
+++ app/src/main/java/com/exampledemo/parsaniahardik/recyclerview_button/Model.java
@@ -0,0 +1,26 @@
+package com.exampledemo.parsaniahardik.recyclerview_button;
+
+/**
+ * Created by hardik on 9/1/17.
+ */
+class Model {
+
+    private int number;
+    private String fruit;
+
+    int getNumber() {
+        return number;
+    }
+
+    void setNumber(int number) {
+        this.number = number;
+    }
+
+    String getFruit() {
+        return fruit;
+    }
+
+    void setFruit(String fruit) {
+        this.fruit = fruit;
+    }
+}
---
+++ app/src/main/java/com/exampledemo/parsaniahardik/recyclerview_button/NextActivity.java
@@ -0,0 +1,25 @@
+package com.exampledemo.parsaniahardik.recyclerview_button;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NextActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+        tv = (TextView) findViewById(R.id.tv);
+
+        for (int i = 0; i < 5; i++) {
+            String text = tv.getText().toString();
+            tv.setText(text + MainActivity.modelArrayList.get(i).getFruit()+" -> "+MainActivity.modelArrayList.get(i).getNumber()+"\n");
+        }
+    }
+}
